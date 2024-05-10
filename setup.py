4913c4994304eb8418db527c28baf18df674a8db
GibranMena <gibran.mena@gmail.com>
2024-05-10 22:55:29 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Scraper_portlet',
+    version='0.1.0',
+    author='Gibran Mena',
+    description='A prect for scraping data from dynamic website using apache portlet',
+    python_requires='>=3',
+    license='GNU General Public License v3',
+    packages=find_packages(),
+)
